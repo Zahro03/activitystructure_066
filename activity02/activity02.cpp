6308d07826de62c4331cf2f81ede4db8607cea0f
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-04-05 11:03:26 +0700

    mengelompokkan beberapa variabel menjadi sebuah tipe data yang baru

---
+++ activity02/activity02.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukan NIM : ";
+	cin.getline(mhs.nim,12);
+	cout << "Masukan Umur : ";
+	cin >> mhs.umur;
+	cin.ignore(1, '\n');
+	cout << "Masukan Nama : ";
+	cin.getline(mhs.nama, 20);
+	cout << "Alamat : ";
+	cout << "\n\tMasukan Desa : ";
+	cin.getline(mhs.alamat.desa, 20);
+	cout << "\tMasukan Kota : ";
+	cin.getline(mhs.alamat.kota, 20);
+
+}
